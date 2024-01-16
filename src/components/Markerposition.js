9b9f42e98e12f889f7d67a3f54e44d3fc0edecaa
The Passionate Programmer <wamungai10@gmail.com>
2024-01-16 03:59:37 +0300

    Create maps layout

--- src/components/Markerposition.js
+++ src/components/Markerposition.js
@@ -1,23 +1,23 @@
-import React ,{useEffect, useMemo} from 'react'
-import {Marker, Popup, useMap} from "react-leaflet"
-import icon from './icon'
+import React, { useEffect, useMemo } from "react";
+import { Marker, Popup, useMap } from "react-leaflet";
+//import icon from "./icon";
 
-function Markerposition({address}) {
-    const position = useMemo(() =>{
-        return [address.location.lat, address.location.lng]
-    },[address.location.lat, address.location.lng])
+function Markerposition({ address }) {
+  const position = useMemo(() => {
+    return [address.location.lat, address.location.lng];
+  }, [address.location.lat, address.location.lng]);
 
-    const map = useMap()
-    useEffect(() =>{
-        map.flyTo(position, 13,{
-            animate:true,
-        })
-    },[map,position])
+  const map = useMap();
+  useEffect(() => {
+    map.flyTo(position, 13, {
+      animate: true,
+    });
+  }, [map, position]);
   return (
-    <Marker icon={icon} position={position}>
-        <Popup>This is the location of the IP Address or Domain</Popup>
+    <Marker  position={position}>
+      <Popup>This is the location of the IP Address or Domain</Popup>
     </Marker>
-  )
+  );
 }
 
-export default Markerposition
+export default Markerposition;
--- src/components/icon.js
+++ src/components/icon.js
@@ -1,9 +1,9 @@
 import L from 'leaflet'
-import icon  from '../../public/images/icon-location.svg'
+import icon  from '../images/icon-location.svg'
 
 export default L.icon({
     iconSize:[32,40],
-    iconANchor:[10,41],
+    iconAnchor:[10,41],
     popupAnchor:[2, -40],
     iconUrl:icon,
 })
